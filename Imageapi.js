ae72607c93d5f00cec17f1d22a15477a7476268b
skc-sketch112 <chunnachud@gmail.com>
2026-03-08 09:05:16 +0530

    Fix image URL in Imageapi.js

--- Imageapi.js
+++ Imageapi.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request) {
 
     const images = [
-      "https://raw.githubusercontent.com/skc-sketch112/Image-sourav-md/refs/heads/main/IMG-20260308-WA0000.jpg"
+      "https://raw.githubusercontent.com/skc-sketch112/Image-sourav-md/main/IMG-20260308-WA0000.jpg"
     ];
 
     const random = images[Math.floor(Math.random() * images.length)];
