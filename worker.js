0b85d463f872b8c35f26ddc7ca25afe82735f244
skc-sketch112 <chunnachud@gmail.com>
2026-03-08 10:47:18 +0530

    Update worker.js with new fetch implementation

--- worker.js
+++ worker.js
@@ -5,7 +5,9 @@ export default {
         "https://raw.githubusercontent.com/skc-sketch112/Image-sourav-md/main/IMG-20260308-WA0000.jpg",
         "https://raw.githubusercontent.com/skc-sketch112/Image-sourav-md/main/712_blond_in.jpg",
         "https://raw.githubusercontent.com/skc-sketch112/Image-sourav-md/main/090_off_and.jpg",
-        "https://raw.githubusercontent.com/skc-sketch112/Image-sourav-md/main/022_pink_taylor.jpg"
+        "https://raw.githubusercontent.com/skc-sketch112/Image-sourav-md/main/022_pink_taylor.jpg",
+        "https://raw.githubusercontent.com/skc-sketch112/Image-sourav-md/refs/heads/main/943_foot_hardcore.jpg",
+        "https://raw.githubusercontent.com/skc-sketch112/Image-sourav-md/refs/heads/main/375_her_big.jpg"
       ];
 
       const random = images[Math.floor(Math.random() * images.length)];
