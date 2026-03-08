a8ddd553f54b0d9c2f4770fdbb5b75b286bb6caa
skc-sketch112 <chunnachud@gmail.com>
2026-03-08 10:13:52 +0530

    Update worker.js

--- worker.js
+++ worker.js
@@ -3,6 +3,9 @@ export default {
 
     const images = [
       "https://raw.githubusercontent.com/skc-sketch112/Image-sourav-md/main/IMG-20260308-WA0000.jpg"
+      "https://raw.githubusercontent.com/skc-sketch112/Image-sourav-md/main/712_blond_in.jpg"
+      "https://raw.githubusercontent.com/skc-sketch112/Image-sourav-md/main/090_off_and.jpg"
+      "https://raw.githubusercontent.com/skc-sketch112/Image-sourav-md/main/022_pink_taylor.jpg"
     ];
 
     const random = images[Math.floor(Math.random() * images.length)];
