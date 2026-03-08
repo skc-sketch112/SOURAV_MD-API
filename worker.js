c34b5fafc4a9c71d7663fbf9b27aa2019bf1b268
skc-sketch112 <chunnachud@gmail.com>
2026-03-08 10:26:47 +0530

    Update worker.js with new image fetching logic

--- worker.js
+++ worker.js
@@ -1,16 +1,19 @@
 export default {
   async fetch(request) {
+    try {
+      const images = [
+        "https://raw.githubusercontent.com/skc-sketch112/Image-sourav-md/main/IMG-20260308-WA0000.jpg",
+        "https://raw.githubusercontent.com/skc-sketch112/Image-sourav-md/main/712_blond_in.jpg",
+        "https://raw.githubusercontent.com/skc-sketch112/Image-sourav-md/main/090_off_and.jpg",
+        "https://raw.githubusercontent.com/skc-sketch112/Image-sourav-md/main/022_pink_taylor.jpg"
+      ];
 
-    const images = [
-      "https://raw.githubusercontent.com/skc-sketch112/Image-sourav-md/main/IMG-20260308-WA0000.jpg",
-      "https://raw.githubusercontent.com/skc-sketch112/Image-sourav-md/main/712_blond_in.jpg",
-      "https://raw.githubusercontent.com/skc-sketch112/Image-sourav-md/main/090_off_and.jpg",
-      "https://raw.githubusercontent.com/skc-sketch112/Image-sourav-md/main/022_pink_taylor.jpg"
-    ];
+      const random = images[Math.floor(Math.random() * images.length)];
 
-    const random = images[Math.floor(Math.random() * images.length)];
+      return fetch(random);
 
-    return fetch(random);
-
+    } catch (error) {
+      return new Response('Error', { status: 500 });
+    }
   }
 }
