1bfc0901667abc45410c20ce0ab92a43ae46cc49
skc-sketch112 <chunnachud@gmail.com>
2026-03-08 09:03:18 +0530

    Add Imageapi module to fetch random images

---
+++ Imageapi.js
@@ -0,0 +1,13 @@
+export default {
+  async fetch(request) {
+
+    const images = [
+      "https://raw.githubusercontent.com/skc-sketch112/Image-sourav-md/refs/heads/main/IMG-20260308-WA0000.jpg"
+    ];
+
+    const random = images[Math.floor(Math.random() * images.length)];
+
+    return fetch(random);
+
+  }
+}
